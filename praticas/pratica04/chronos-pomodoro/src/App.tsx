da683b97831a761b7a224a71a0777de6abd9286d
irn64 <gabriel.for3@outlook.com>
2026-03-26 21:50:13 -0300

    Até a pratica20

--- praticas/pratica04/chronos-pomodoro/src/App.tsx
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -1,3 +1,60 @@
-export function App(){
-    .
+import { Container } from './components/Container';
+import { Logo } from './components/logo';
+import { Menu } from './components/menu';
+import { CountDown } from './components/countdown';
+import { DefaultInput } from './components/defaultImput';
+import { Cycles } from './components/cycles';
+import { DefaultButton } from './components/defaultButtom';
+import { Footer } from './components/footer';
+
+import { PlayCircleIcon } from 'lucide-react';
+
+import './styles/theme.css';
+import './styles/globals.css';
+
+export function App() {
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p></p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            {/* Mantivemos apenas o botão principal de Play */}
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
+      </Container>
+
+      {/* Nosso novo rodapé entra aqui, no seu próprio Container! */}
+      <Container>
+        <Footer />
+      </Container>
+    </>
+  );
 }
--- praticas/pratica04/chronos-pomodoro/src/main.tsx
+++ praticas/pratica04/chronos-pomodoro/src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <>
-      console.log('oi')
-      <h1>Bem-vindo a primeira aula de ReactJs</h1>
-      <h2>Isso ai</h2>
-    </>
+    <App />
   </StrictMode>,
 );
